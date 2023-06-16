e27047dd3b7e4b6f106b8cda19c5c910eed62fa4
Abdul Karim <karim.develops@gmail.com>
2023-06-16 09:17:17 +0500

    Add a heading and create a layout in app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QGridLayout
 from PyQt6.QtGui import QIcon
 
 class MainWindow(QMainWindow):
@@ -11,6 +11,17 @@ class MainWindow(QMainWindow):
         self.setWindowIcon(QIcon("img/logo.png"))
 
         self.setFixedSize(850, 150)
+
+        heading = QLabel("Caesar Cipher")
+
+        layout = QGridLayout()
+        widget = QWidget()
+
+        layout.addWidget(heading, 0, 1)
+
+        widget.setLayout(layout)
+
+        self.setCentralWidget(widget)
 
 app = QApplication([])
 app.setStyleSheet(Path('style.css').read_text())
