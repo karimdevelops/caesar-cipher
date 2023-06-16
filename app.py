2ceb0c3e76ba3de690299095cf35ccc39173b44e
Abdul Karim <karim.develops@gmail.com>
2023-06-16 14:59:18 +0500

    Add input layout for crpytography in app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QGridLayout, QPushButton
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QLabel, QGridLayout, QPushButton, QLineEdit, QVBoxLayout
 from PyQt6.QtGui import QIcon
 
 class MainWindow(QMainWindow):
@@ -19,15 +19,34 @@ class MainWindow(QMainWindow):
         decrypt_button = QPushButton("Decryption")
 
         layout = QGridLayout()
-        widget = QWidget()
+        self.widget = QWidget()
 
         layout.addWidget(heading, 0, 0)
         layout.addWidget(encrypt_button, 1, 0)
         layout.addWidget(decrypt_button, 1, 1)
 
-        widget.setLayout(layout)
+        self.widget.setLayout(layout)
 
-        self.setCentralWidget(widget)
+        self.setCentralWidget(self.widget)
+
+        encrypt_button.released.connect(self.input_layout)
+        decrypt_button.released.connect(self.input_layout)
+
+    def input_layout(self):
+        continue_button = QPushButton("Continue")
+        user_input = QLineEdit("")
+        user_input.setPlaceholderText("Enter your text...")
+
+        layout = QVBoxLayout()
+        
+        layout.addWidget(user_input)
+        layout.addWidget(continue_button)
+
+        self.widget = QWidget()
+
+        self.widget.setLayout(layout)
+
+        self.setCentralWidget(self.widget)
 
 app = QApplication([])
 app.setStyleSheet(Path('style.css').read_text())
