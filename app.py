9f8ada20c46fcd0e3aa541041083597f6af6b269
Abdul Karim <karim.develops@gmail.com>
2023-06-18 22:54:42 +0500

    Set content margins of input layout in app.py

--- app.py
+++ app.py
@@ -71,6 +71,8 @@ class MainWindow(QMainWindow):
         layout.addWidget(user_input)
         layout.addWidget(continue_button)
 
+        layout.setContentsMargins(25, 60, 0, 0)
+
         self.widget = QWidget()
 
         self.widget.setLayout(layout)
